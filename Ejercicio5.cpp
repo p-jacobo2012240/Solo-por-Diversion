976848b9fef3f8000f4362887991b3efb4a67448
Pablo Jacobo <pablojacobo1998@gmail.com>
2018-10-12 13:01:25 -0600

    Arreglos y vectores

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -3,39 +3,53 @@
 using namespace std;
 
 float operar_valores(){
-
-	//Arreglo bidimensional
-   	string datos_empleados[2][2];  //libros
-    
-    //Valores entrantes
-	string n_empleado;  //titulo
-	int salario; //autor
 	
-	cout << "Ejercicio de Arrays:    \n";
-    
-    for(int i = 0; i < 2; i++)
-    {
-        cout << "\n******* Datos " << i + 1 << "********:\n";
-        cout << "Empleados Nombre: ";
-        getline(cin, n_empleado);
-        cout << "Salario: ";
-        cin>>salario;
-        cin.ignore();
-        datos_empleados[i][0] = n_empleado;
-        datos_empleados[i][1] = salario;
-        
-       
-    }
-     float x = datos_empleados[0][0];
+	//Definiendo variables
+	string nombre_empleado;
+	int salario[10000];
+	
+	//Definiendo array de empleados
+	int arreglo3[5];
+	string array_empleados[5];
+	
+	for (int i=0;i<5; i++){
+	
+	
+       cout<<"-----Ingrese Dato----  "<<i<<"\n";
 	  	
-     cout<<"los datos son: "<<x<<endl;
-     cout<<"los datos son: "<<datos_empleados[0][1]<<endl;
-     cout<<"El multiploicacion es"<<
+		cout<<"ingrese nombre \t";
+	    getline(cin,nombre_empleado);
+               
+        cout<<"ingrese edad \t";
+	    cin>>salario[i];
+        cin.ignore();
 
+	 
 
-    system("pause");
+	   array_empleados[i]= nombre_empleado;
+	   
+    }
+    
+    //Mostrando los valores
+    
+    for (int i=0;i<5; i++)
+	{
+     
+       cout<<"los datos son \n";
+	   cout<<"\t"<<salario[i];
+       cout<<"\t"<<array_empleados[i];
+       cout<<endl;
+    }
+    
+    
+    //Suma totral de salarios
+    cout<<"ARREGLO RESULTANTE"<<endl;
+    for(int i=0;i<1;i++){
+        arreglo3[i] = salario[0] + salario[1] + salario[2] + salario[3] + salario[4];
+        cout<<arreglo3[i]<<endl;
+    }
+    
 
-    return 0;
 }
 
 int main(){

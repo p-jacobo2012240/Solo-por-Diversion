c0023159fd9e8eae903eefe9bd7304afe299e632
unknown <pablojacobo1998@gmail.com>
2018-10-12 19:13:06 -0600

    Ultimo cambio

--- Ejercicio5.cpp
+++ Ejercicio5.cpp
@@ -14,15 +14,17 @@ float operar_valores(){
 	
 	for (int i=0;i<5; i++){
 	
-	
-       cout<<"-----Ingrese Dato----  "<<i<<"\n";
+	    	 	
+       cout<<"Informacion Empleado N0: "<<i<<"\n";
 	  	
-		cout<<"Ingrese un nombre:   \t";
+		cout<<"Ingrese un nombre: \t";
 	    getline(cin,nombre_empleado);
-               
-        cout<<"Ingrese su salario:  \t";
+       
+		       
+        cout<<"Ingrese su salario: \t";
 	    cin>>salario[i];
         cin.ignore();
+        cout<<""<<endl;
 
 	 
 
@@ -31,40 +33,38 @@ float operar_valores(){
     }
     
     //Mostrando los valores
+	cout<<"N0:   Nombre:      Sueldo: \n";
     for (int i=0;i<5; i++){
 	
-	
-     
-       cout<<"los datos son \n";
+	   
+	   cout<<i;
+	   cout<<"\t"<<array_empleados[i];
 	   cout<<"\t"<<salario[i];
-       cout<<"\t"<<array_empleados[i];
-       cout<<endl;
-    }
-    
+       cout<<" "<<endl;
+	          
+	}
+	cout<<""<<endl;
     
     //SUMA DE TODA LA PLANILLA
-    cout<<"ARREGLO RESULTANTE"<<endl;
     for(int i=0;i<1;i++){
         arreglo3[i] = salario[0] + salario[1] + salario[2] + salario[3] + salario[4];
-        cout<<arreglo3[i]<<endl;
+        cout<<"SUMA PLANILLA: "<<arreglo3[i]<<endl;
     }
     
     //PROMEDIO DE LA PLANILLA
-    cout<<"Promedio de la planilla"<<endl;
     for(int i=0; i<1; i++ ){
     	int resultado;
 		arreglo3[i] = salario[0] + salario[1] + salario[2] + salario[3] + salario[4];
 		resultado = arreglo3[i] / 5; //Cambiar por contantes
-        cout<<resultado<<endl;	
+        cout<<"PROMEDIO:      "<<resultado<<endl;	
 	}
 	
 	//IGSS
-	cout<<"Calculo del IGGS"<<endl;
-    for(int i=0; i<1; i++){
+	for(int i=0; i<1; i++){
     	int igss;
     	arreglo3[i] = salario[0] + salario[1] + salario[2] + salario[3] + salario[4];
 		igss = arreglo3[i] * 0.483; //Cambiar por contantes
-		cout<<igss<<endl;
+		cout<<"IGGS 4.83%:    "<<igss<<endl;
 	}
 
 }

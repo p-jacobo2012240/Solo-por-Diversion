4263a09c7c5536670c51f412737fec97b98c08b1
unknown <pablojacobo1998@gmail.com>
2018-11-02 16:09:59 -0600

    Factura Success

--- jugadores.cpp
+++ jugadores.cpp
@@ -1,36 +1,23 @@
 #include <iostream>
 
 using namespace std;
+int main()
+{
+    string libros[5][2];
+    cout << "Por favor ingrese la siguiente información de los Libros: \n";
+    string titulo ,autor;
+    for(int i = 0; i < 5; i++)
+    {
+        cout << "\n******* Libro " << i + 1 << "********:\n";
+        cout << "Titulo: ";
+        getline(cin,titulo);
+        cout << "Autor: ";
+        getline(cin,autor);
+        libros[i][0] = titulo;
+        libros[i][1] = autor;
+    }
 
-int main(){
-	
-	string productos[100];
-	int  op = 0;  
-	int i =0;
-	while( op != 2 ){
-		//system("cls");
-		cout<<"1. Ingresar "<<endl;
-		cout<<"2. Mostrar  "<<endl;
-		string nombre;
-		
-		switch(op){
-			case 1: 
-			cout<<"Ingrese el nombre del producto"<<endl;
-			cin>>nombre;
-			productos[i] = nombre;
-			//i++;
-			break;
-			
-			case 2:
-			for(int j=0; j<i; j++ ){
-				cout<<"->"<<(j + 1)<<productos[j]<<endl;
-			}
-			break;
-				default:
-					cout<<"Marque una opcion valida"<<endl;
-					
-		}
-		op++;	
-	}
-	
+    system("pause");
+
+    return 0;
 }

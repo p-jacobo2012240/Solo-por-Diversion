cc625fb3b1cfbf52b00af96aaa9a4b743e7c6e0a
unknown <pablojacobo1998@gmail.com>
2018-11-02 19:25:23 -0600

    ESTADISTICA FINISHED

--- jugadores.cpp
+++ jugadores.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
-
+#define MAX 100
 using namespace std;
-int main()
-{
-    string libros[5][2];
-    cout << "Por favor ingrese la siguiente información de los Libros: \n";
-    string titulo ,autor;
-    for(int i = 0; i < 5; i++)
-    {
-        cout << "\n******* Libro " << i + 1 << "********:\n";
-        cout << "Titulo: ";
-        getline(cin,titulo);
-        cout << "Autor: ";
-        getline(cin,autor);
-        libros[i][0] = titulo;
-        libros[i][1] = autor;
+
+int main(){
+    /** Escriba un programa que pida al usuario los datos
+        de dos matrices de NxN y luego muestre su suma.
+    **/
+    int A[MAX][MAX];
+    int B[MAX][MAX];
+    int C[MAX][MAX];
+    int N;
+
+    cout<<"Ingrese el valor de N:";
+    cin>>N;
+
+    cout<<"MATRIZ A"<<endl;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout<<"["<<i<<"]["<<j<<"]:";
+            cin>>A[i][j];
+        }
+    }
+    cout<<"MATRIZ B"<<endl;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout<<"["<<i<<"]["<<j<<"]:";
+            cin>>B[i][j];
+        }
     }
 
-    system("pause");
+    cout<<"MATRIZ SUMA"<<endl;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            C[i][j] = A[i][j] + B[i][j];
+            cout<<C[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
+

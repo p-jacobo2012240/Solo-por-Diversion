98190617ca2b498e23752ac99c29027ec5d94573
unknown <pablojacobo1998@gmail.com>
2018-11-23 20:42:37 -0600

    Ultimos cambios

--- estructuras -reportes/practicados.cpp
+++ estructuras -reportes/practicados.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <conio.h>
 #include <string.h>
+#define IVA  0.12
 
 using namespace std;
 
@@ -25,7 +26,6 @@ void imprimir_factur();
 int main(){
 	
 	int op = 0;
-	
 	do{
 		
 		cout<<"   Ingreso de Factura     "<<endl;
@@ -63,10 +63,93 @@ int main(){
 	return 0;
 }
 
+//Funcion calculo de monto
+float calc_monto(int x, float y ){
+	float z = x + y;
+	return z;
+}
+
+//Funcion calcular IVA
+float calc_iva(float d){
+	float v = d * IVA;
+	return v;
+}
+
+//Funcion calcular total
+float calc_total(float q, float u){
+	float l = q + u;
+	return l;
+}
+
 void ingresar_fatura(){
-	cout<<"factura"<<endl;
+	
+	char espacio[2];
+	Factura _fac;
+	
+	cin.getline (espacio, 2 );
+
+	cout <<"Fecha: ";
+	cin >> _fac.fecha , '\n';
+	
+	cout <<"Numero Factura: ";
+	cin >> _fac.numero_factura, '\n';
+	cin.ignore();
+	
+	cout <<"Nombres y apellidos: ";
+	cin.getline( _fac.nombre_apellido, 30, '\n' );
+
+	cout<<"Nit: ";
+	cin.getline( _fac.nit, 30, '\n' );
+	
+	
+	cout<<"Concepto: ";
+	cin.getline( _fac.concepto, 30, '\n' );
+	
+	cout <<"Cantidad: ";
+	cin >> _fac.cantidad , '\n';
+
+	cout <<"Precio Unitario: ";
+	cin >> _fac.precio_unitario , '\n';
+	
+	_fac.monto = calc_monto(_fac.cantidad, _fac.precio_unitario );
+
+	_fac.iva =  calc_iva(_fac.monto);
+	
+	_fac.total = calc_total(_fac.monto, _fac.iva );
+	
+	fstream archivo;
+	archivo.open( "factReporte.bin", ios::out | ios::app | ios::binary );
+	archivo.write( ( char * ) & _fac, sizeof( Factura ) );
+	archivo.close();
+	cout << "\n";
+	cout << "Datos Guardado";
+	cout<<" "<<endl;
+	//main();
 }
 
 void imprimir_factur(){
-	cout<<"reporte"<<endl;
+
+	Factura mostrar;
+	fstream archivo( "factReporte.bin", ios::in | ios::binary );
+	if  (archivo.fail() ) {
+		cerr << "Error al abrir factReporte.bin" << endl;
+	} else {
+		system( "cls" );
+		cout<<"  Fecha "<<"  "<<" Numero Fac"<<"  "<<" Nombre "<<"  "<<" Nit "<<" Monto "<<"  "<<" Iva Monto " <<endl;
+		while ( !archivo.eof() ) {
+			archivo.read( ( char* ) & mostrar, sizeof( Factura ));
+			if ( !archivo.eof() ) {
+				if ( mostrar.numero_factura > 10 ) {
+					
+					cout <<mostrar.fecha<<"   "<< mostrar.numero_factura<<"   "<< mostrar.nombre_apellido<<"   "<< mostrar.nit<<"   "<<mostrar.monto<<"   "<< mostrar.iva;			
+					cout << "\n";
+				}
+			}
+		}
+		cout << "\n";
+		cout << "Presione enter para regresar al menu";
+	}
+	archivo.close();
+	cout<<" "<<endl;
+
 }

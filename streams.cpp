3a8f24d5a776587f7b8713119b0a3415abb856a3
unknown <pablojacobo1998@gmail.com>
2019-02-08 22:41:44 -0600

    Streams DB Fake Read

--- streams.cpp
+++ streams.cpp
@@ -6,10 +6,19 @@ using namespace std;
 
 int main(){
 	
-	ofstream saludo;
+	//Escritura
+	/*ofstream saludo;
 	saludo.open("prueba.text");
 	
 	saludo<<"hola devs"<<endl;	
 	cout<<"Hola de nuevo"<<endl;
+	saludo.close();*/
+	
+	//Lectura
+	ifstream saludo;
+	char line[80];
+	saludo.open("prueba.text");
+	saludo.getline(line, 80  );
+	cout<<line<<endl;
 	saludo.close();
 }

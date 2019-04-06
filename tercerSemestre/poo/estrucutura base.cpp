9e726f9023dfca35ead50ca9fa0211fba79909a5
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-04-05 22:06:35 -0600

    Base code

--- tercerSemestre/poo/estrucutura base.cpp
+++ tercerSemestre/poo/estrucutura base.cpp
@@ -4,24 +4,70 @@ using namespace std;
 
 //clase base para las demas
 
-class Data{
+class Salas{
 	public:
-		char nombre[30];
-		char apellido [30];
-		char dirreccion [30];
+		int numero_sala;
+		char capacidad[30];
 };
 
 
-class Complemento{
+class Peliculas{
 	public:
-		char  descripcion[30];
-		Data d;
+		char nombre_peli[30];
+		char genero[30];
+		char categoria[30];
+		Salas sala;
+		//Metodos
+		void mostrarPeliculas();
+		void ingresarPeliculas();
 };
+
+
+void Peliculas::mostrarPeliculas(){
+	
+	cout<<"Nombre peli "<<nombre_peli<<endl;
+	cout<<"Genero      "<<genero<<endl;
+	cout<<"Categoria   "<<categoria<<endl;
+	cout<<"Sala :: Capacidad "<<sala.capacidad<<endl;
+	cout<<"Sala :: Numero    "<<sala.numero_sala<<endl;
+
+}
+
+void Peliculas::ingresarPeliculas(){
+	Peliculas p;
+	
+	cout<<"Nombre peli "; 
+		cin>>p.nombre_peli;
+		cin.ignore();
+	cout<<"Genero      "; 
+		cin>>p.genero;
+		cin.ignore();
+	cout<<"Categoria   "; 
+		cin>>p.categoria;
+		cin.ignore();
+	cout<<"Sala :: Capacidad "; 
+		cin>>p.sala.capacidad;
+		cin.ignore();
+	cout<<"Sala :: Numero    "; 
+		cin>>p.sala.numero_sala;
+		cin.ignore();
+	
+	cout<<" "<<endl;
+	p.mostrarPeliculas();
+	cout<<" "<<endl;
+	
+}
+
+
 
 
 
 int main(){
 	
-	cout<<"Hola"<<endl;
+	Peliculas peli;
+	
+	peli.ingresarPeliculas();
+	
+	//cout<<"Hola"<<endl;
 	
 }

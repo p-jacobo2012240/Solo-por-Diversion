fba10bf81452dbed9408459154e497efde6d4c35
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-04-25 22:50:48 -0600

    [Object Hck-Configure] : build...

---
+++ tercerSemestre/lib/library_wip.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#define LIMIT 3
+
+using namespace std;
+
+class Hck{
+	public:
+		char nombre[30];
+		
+};
+
+
+void insertObj(Hck hck ); //Handler method
+Hck requireHck();
+
+int main(){
+	
+	int op;
+	
+	do{
+		
+		cout<<"============================="<<endl;
+		
+		
+		cout<<"============================="<<endl;
+		
+	}while(op != LIMIT );
+	
+}

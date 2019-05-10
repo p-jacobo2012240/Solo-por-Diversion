7f62c4c51eebb6e3a6bf7f7bd19f9caef1d62329
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-05-09 22:34:22 -0600

    1

---
+++ tercerSemestre/constructores/1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class Persona{
+	private:
+		string nombre;
+		int edad;
+		string genero;
+	public:
+		Persona(string nom, int e,string gen){
+			nombre = nom;
+			edad = e;
+			genero = gen;
+		}		 
+};
+
+int main(){
+	
+	string n, g;
+	int e;
+	
+	cout<<"ingrese nombre : ";
+	cin>>n;
+	cout<<"ingrese edad   : ";
+	cin>>e;
+	cout<<"ingrese genero : ";	
+	cin>>g;
+	
+	Persona unaPersona(n,e,g);
+	
+	
+	
+	//cout<<"hola constructores"<<endl;
+	
+}

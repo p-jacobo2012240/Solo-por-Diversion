50b4d0ac5d8d912318ff8d884aabf166cf1103fa
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-05-17 23:34:28 -0600

    menus

--- tercerSemestre/proyecto-final/proyecto.cpp
+++ tercerSemestre/proyecto-final/proyecto.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define MAX 6
 
 using namespace std;
 
@@ -15,7 +16,28 @@ class Habitacion{
 		int fin_reservacion
 };
 
+void segundo_menu(){
+	
+	cout<<" a. catalogo           "<<endl;
+	cout<<" b. habitaciones       "<<endl;
+	cout<<" c. tipo_habitaciones  "<<endl;
+	cout<<" d. reservacion        "<<endl;
+}
+
 
 int main(){
+	
+	int op;
+	
+	do{
+		
+		cout<<"==========================="<<endl;
+		cout<<"| 1. Ingreso de datos     |"<<endl;
+		cout<<"| 2. Impresion pantalla   |"<<endl;
+		cout<<"| 3. Ticket               |"<<endl;
+		cout<<"==========================="<<endl;
+		
+	}while(op != MAX );
+	
 	
 }

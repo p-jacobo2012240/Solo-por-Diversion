0bf1a06b7e5562fe910845903de786ea125b7226
p-jacobo2012240 <pablojacobo1998@gmail.com>
2019-05-17 23:14:58 -0600

    primera parte

---
+++ tercerSemestre/proyecto-final/proyecto.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+class Typo{
+	public:
+		string nombre_typo;
+		float costo;
+};
+
+class Habitacion{
+	public:
+		int numero_habitacion;
+		int inicio_reservacion;
+		int fin_reservacion
+};
+
+
+int main(){
+	
+}

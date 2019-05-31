fdf765e5aa7d18263b10dbc24d5dd77bc9395b0f
Pablo Jacobo <pablojacobo1998@gmail.com>
2019-05-31 13:41:00 -0600

    show elementets in stack --success

--- tercerSemestre/stacks and tails/stack_one.cpp
+++ tercerSemestre/stacks and tails/stack_one.cpp
@@ -2,9 +2,60 @@
 
 using namespace std;
 
+class Count{
+	public:
+		int dato;
+		Count* nextDato;
+		//procedures
+		void addStack(Count*&, int);
+		void showStack( Count*&, int &);
+};
+
+
+void Count::addStack(Count*&stack, int deferred){
+	Count *newStack = new Count();
+	newStack->dato = deferred;
+	newStack->nextDato = stack;
+	stack = newStack;
+	
+	cout<<"element added to : "<< deferred  <<" existent stack"<<endl;
+	
+}
+
+void Count::showStack(Count*&stack, int &useDeferred){
+	Count *q  = stack;
+	useDeferred = q->dato;
+	stack = q->nextDato;
+	delete q;
+}
+
 int main(){
 	
+	//cout<<" 1. Stacks in C++  "<<endl;
 	
-	cout<<" 1. Stacks in C++  "<<endl;
+	int opt;
+	int n;
+	Count *stack = NULL;
+	Count instenceCount;
+	
+	cout<<"Size stack :"<<endl; 
+	cin>>opt;
+	
+	for(int i= 0; i<opt; i++){
+		
+	
+		cout<<"Added your number favorite"<<endl;
+		cin>>n;
+		
+		instenceCount.addStack( stack, n );
+	}
+	
+	
+	while( stack != NULL ){
+		cout<<"::: fixes  :::"<<endl;
+		instenceCount.showStack(stack, n);
+	}
+	
+	
 	
 }
